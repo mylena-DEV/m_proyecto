e90cf0c453d3a1f9fc322e309e306be48e1fa6aa
mylena-DEV <mylenalo403@gmail.com>
2024-08-20 17:55:24 -0500

    mis datos personales
    hecho

---
+++ datos_U.py
@@ -0,0 +1,3 @@
+NombreU = "Universidad Tecnica de cotopaxi"
+ubicacion = "virgen del cisne"
+carreras = "contabilidad, sistemas, turismo, electromecanica"
---
+++ mis_datos.py
@@ -0,0 +1,5 @@
+Nombre = "mylena"
+apellido = "Lozada"
+correo = "mylenalo403@gmail.com"
+telefono = 0969703964
+edad = 23
