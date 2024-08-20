1d96742b7b8613df5ea3b4510e17c9d4f47d217f
mylena-DEV <mylenalo403@gmail.com>
2024-08-20 18:14:58 -0500

    Cambio_DU

--- mis_datos.py
+++ mis_datos.py
@@ -1,7 +1,13 @@
 Nombre = "mylena"
 apellido = "Lozada"
 correo = "mylenalo403@gmail.com"
-telefono = 0969703964
 edad = 23
 estatura = 1.52
-hbcawdg
+
+#cambio 
+
+NombreU = "Universidad Tecnica de cotopaxi"
+ubicacion = "virgen del cisne"
+carreras = "contabilidad, sistemas, turismo, electromecanica"
+telefono = 123456789
+
